d9902516fdd7046d84d53f3f7ca4c9980bcea14a
Lee <lee-pham@users.noreply.github.com>
2016-05-15 01:51:26 -0500

    Update featureNormalize.m

--- featureNormalize.m
+++ featureNormalize.m
@@ -1,16 +1,13 @@
 function [X_norm, mu, sigma] = featureNormalize(X)
 %FEATURENORMALIZE Normalizes the features in X 
 %   FEATURENORMALIZE(X) returns a normalized version of X where
-%   the mean value of each feature is 0 and the standard deviation
-%   is 1. This is often a good preprocessing step to do when
-%   working with learning algorithms.
+%   the mean value of each feature is 0 and the standard deviation is 1.
 
 X_norm = X;
 mu = zeros(1, size(X, 2));
 sigma = zeros(1, size(X, 2));
-[~, n] = size(X);
 
-for i = 1:n
+for i = 1:size(X, 2)
     X_norm(:,i) = (X(:,i)-mean(X(:,i)))/std(X(:,i));
 end
 
