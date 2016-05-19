a089599a0ff51372e8b847147512998b4863bf96
Lee <lee-pham@users.noreply.github.com>
2016-05-18 22:57:25 -0500

    Update gram.m

--- gram.m
+++ gram.m
@@ -1,9 +1,10 @@
-function out = gram(A)
-
-out = A;
-
-for i = 1:size(A, 2)
-    out(:,i) = A(:,i)
+function u = gram(v)
+u = zeros(size(v));
+u(:,1) = v(:,1);
+for i = 2:size(v, 2)
+    u(:,i) = v(:,i);
+    for j = i:size(v, 2)
+        u(:,i) = v(:,j);
+    end
 end
-
 end
