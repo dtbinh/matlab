984a9318e06b957c5a89e08a3a62005540f66e3a
Lee <lee-pham@users.noreply.github.com>
2016-05-24 00:59:54 -0500

    readability

--- lego/PID.m
+++ lego/PID.m
@@ -1,22 +1,19 @@
-ev3 = legoev3('usb');
+clear
+ev3 = legoev3;
 beep(ev3)
-clearLCD(ev3)
 gyro = gyroSensor(ev3);
-angle = readRotationAngle(gyro);
 resetRotationAngle(gyro)
 
 motor1 = motor(ev3, 'A');
 motor2 = motor(ev3, 'D');
-
 motor1.Speed = 0;
 motor2.Speed = 0;
-
 start(motor1)
 start(motor2)
 
-dt = 0.000001;
-Kp = 3; Ki = 10; Kd = .00001;
-setpoint = 0;
+Kp = 5; Ki = 00; Kd = 0;
+dt = 0.001;
+setpoint = readRotationAngle(gyro);
 previous_error = 0;
 integral = 0; 
 
@@ -27,15 +24,14 @@ while ~readButton(ev3, 'up')
     integral = integral + error*dt;
     derivative = (error - previous_error)/dt;
     output = Kp*error + Ki*integral + Kd*derivative;
+    previous_error = error;
+    disp([measured_angle output])
+   
     motor1.Speed = -output;
     motor2.Speed = -output;
-    previous_error = error;
     pause(dt)
     
 end
 
 stop(motor1)
 stop(motor2)
-clear
-  
-  
