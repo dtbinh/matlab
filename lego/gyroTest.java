e9cf2a6a6c85781ceb1fa8c7699de1b925e9e149
Lee <lee-pham@users.noreply.github.com>
2016-05-26 00:52:05 -0500

    Update gyroTest.java

--- lego/gyroTest.java
+++ lego/gyroTest.java
@@ -1,30 +1,49 @@
+import lejos.hardware.ev3.LocalEV3;
+import lejos.hardware.lcd.Font;
+import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.utility.Delay;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.SampleProvider;
 
 public class LEGO {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		GraphicsLCD gLCD = LocalEV3.get().getGraphicsLCD();
+		final int SW = gLCD.getWidth();
+	    final int SH = gLCD.getHeight();
+		gLCD.setFont(Font.getDefaultFont());
+		int L = 1;
+		int r = 1;
+		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
+		SampleProvider touch = touchSensor.getMode("Touch");
+		float[] sample = new float[touch.sampleSize()];
+		touch.fetchSample(sample, 0);
 		EV3GyroSensor gyro = new EV3GyroSensor(SensorPort.S4);
-		Delay.msDelay(2000);
 		gyro.reset();
 		float[] angleAndRate = new float[2];
 		gyro.getAngleAndRateMode().fetchSample(angleAndRate, 0);
-		LCD.drawString(angleAndRate[0] + " " + angleAndRate[1], 0, 0);
 		
-		for(int i = 0; i < 1000; i += 1){
+		while (sample[0] == 0){
 			
 			gyro.getAngleAndRateMode().fetchSample(angleAndRate, 0);
-			LCD.drawString(angleAndRate[0] + " " + angleAndRate[1], 0, 0);
 			
+			float rate = (float) (-180*L*angleAndRate[1]*angleAndRate[0]/(3.14*r));
+			int round = Math.round(rate);
+			LCD.clear();
+			gLCD.drawString("dtheta/dt = " + round, SW/2, SH/2, GraphicsLCD.BOTTOM | GraphicsLCD.HCENTER);
+			LCD.refresh();
+			
+			
+			touch.fetchSample(sample, 0);
+
 		}
 		
 		gyro.close();
+		touchSensor.close();
 		
 		}
 	
 	}
-
-
