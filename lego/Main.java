0646211f6ede86c37fb65d9e51bdee92a2e80130
Lee <lee-pham@users.noreply.github.com>
2016-05-26 03:22:20 -0500

    Update Main.java

--- lego/Main.java
+++ lego/Main.java
@@ -1,13 +1,26 @@
+//Necessary imports
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.GraphicsLCD;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.robotics.RegulatedMotor;
 import lejos.robotics.SampleProvider;
+import lejos.utility.Delay;
 
 public class LEGO {
 
 	public static void main(String[] args) {
+		
+	    //System constants
+	    final float L = (float) 16.5;
+		final float r = (float) 4.7;
+		
+		//Motor set-up
+		RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
+		RegulatedMotor motorD = new EV3LargeRegulatedMotor(MotorPort.D);
 		
 		//Touch sensor set-up
 		EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S1);
@@ -25,19 +38,30 @@ public class LEGO {
 		GraphicsLCD gLCD = LocalEV3.get().getGraphicsLCD();
 		final int SW = gLCD.getWidth();
 	    final int SH = gLCD.getHeight();
-		
-	    //System constants
-	    final int L = 1;
-		final int r = 1;
-		
+	
 		while (touchValue[0] == 0){
 			
 			gyroSensor.getAngleAndRateMode().fetchSample(gyroValues, 0);
-			float rate = (float) (-180*L*gyroValues[1]*gyroValues[0]/(Math.PI*r));
-			int round = Math.round(rate);
+			float rate = (float) (180*L*Math.sin(Math.toRadians(gyroValues[1]))*gyroValues[0]/(Math.PI*r));
+			
 			gLCD.clear();
-			gLCD.drawString("dtheta/dt = " + round, SW/2, SH/2, GraphicsLCD.BOTTOM | GraphicsLCD.HCENTER);
+			gLCD.drawString(gyroValues[0] + " " + gyroValues[1], 0, 0, 0);
+			gLCD.drawString("dtheta/dt = " + rate, SW/2, SH/2, GraphicsLCD.BOTTOM | GraphicsLCD.HCENTER);
 			gLCD.refresh();
+			
+			motorA.setSpeed((int) rate);
+			motorD.setSpeed((int) rate);
+			if (gyroValues[1] > 0){
+				
+				motorA.backward();
+				motorD.backward();
+		
+			}else {
+				
+				motorA.forward();
+				motorD.forward();
+				
+			}
 			
 			touchSensor.fetchSample(touchValue, 0);
 
@@ -45,6 +69,8 @@ public class LEGO {
 		
 		gyroSensor.close();
 		touchSensor.close();
+		motorA.close();
+		motorD.close();
 		
 		}
 	
