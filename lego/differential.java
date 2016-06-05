3a666e283a7baae659e6f6c10a6ea819aff6bd6d
Lee <lee-pham@users.noreply.github.com>
2016-06-05 13:33:44 -0500

    Update differential.java

--- lego/differential.java
+++ lego/differential.java
@@ -1,34 +1,33 @@
 //Necessary imports
 import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.GraphicsLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.robotics.RegulatedMotor;
-import lejos.robotics.SampleProvider;
 
 public class LEGO {
 
 	public static void main(String[] args) {
 		
-	  //System constants
-	  final float r = (float) 9;
+		//System constants
+		final float r = (float) 9;
 		final float R = (float) 10;
-
+		final float accelA = float (100);
+		
 		//Motor set-up
 		RegulatedMotor motorA = new EV3LargeRegulatedMotor(MotorPort.A);
 		RegulatedMotor motorD = new EV3LargeRegulatedMotor(MotorPort.D);
 		
-		
-		motorA.setAcceleration(R/r);
-		motorB.setAcceleration(r/R);
+		motorA.setSpeed(1000);
+		motorD.setSpeed(1000);
+		motorA.setAcceleration(accelA);
+		motorD.setAcceleration((r/R)*accelA);
 		motorA.forward();
-		motorB.forward();
+		motorD.forward();
+		
 		
 		motorA.close();
 		motorD.close();
+		
 		
 	}
 	
